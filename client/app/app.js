f0dfdfe8446964b9de3bbbf216847f9e9f51ecef
Noel Felix <nfelix83@gmail.com>
2016-02-13 14:27:31 -0800

    All tests passing

--- client/app/app.js
+++ client/app/app.js
@@ -7,6 +7,9 @@ angular.module('shortly', [
 ])
 .config(function($routeProvider, $httpProvider) {
   $routeProvider
+    // check for auth
+      // on success, next
+      // on fail, redirect to signin
     .when('/signin', {
       templateUrl: 'app/auth/signin.html',
       controller: 'AuthController'
@@ -15,8 +18,19 @@ angular.module('shortly', [
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
     })
-    // Your code here
-
+    .when('/links', {
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController',
+      authenticate: true
+    })
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController',
+      authenticate: true
+    })
+    .otherwise({
+      redirectTo: '/links'
+    });
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
     $httpProvider.interceptors.push('AttachTokens');
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -16,25 +16,34 @@ angular.module('shortly.services', [])
   };
   
   var addLink = function(link){
-      $http({
+    $http({
         method: 'POST',
         url: '/api/links',
         data: link
       }).then(function successCallback(data){
-        console.log('addLink gets server response: ' + data.data);
+        console.log('addLink gets server response: ' + data.status);
       }, function errorCallback(err){
         console.error(err);
       });
   };
 
-  // var validateLink = function(){};
+  var validateLink = function(){
+  };
+
+  //regex to validate URL:
+  var isValidUrl = function(url){
+    console.log('Validating');
+    var validUrlTest =  /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return validUrlTest.test( url );    
+  }; 
 
   return {
     getLinks: getLinks,
-    addLink: addLink
+    addLink: addLink,
+    isValidUrl: isValidUrl
   };
 
-})
+}) //Factory end
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
